5bb99a3faa568a3e806e64da1b9832e80df5f90e
Nguyen Hoang Thach <t3ngho00@students.oamk.fi>
2025-01-14 17:52:52 +0200

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Button } from '@chakra-ui/react'
-
+import { Button, GridItem, Show } from '@chakra-ui/react'
+import { Grid } from '@chakra-ui/react'
 function App() {
-  return (<div><Button colorPalette={'blue'}>Click me</Button></div>)
+  return (
+    <Grid templateAreas={{
+      base: `"nav" "main"`,
+      lg: `"nav nav" "aside main"`
+    }}>
+       <GridItem area={"nav"} bg={"coral"}>nav</GridItem>
+       <GridItem area={"aside"} bg="orange" hideBelow={"lg"}>Aside</GridItem>
+       
+       <GridItem area={"main"} bg="violet">Main</GridItem>
+    </Grid>
+   
+  )
 }
 
 export default App
