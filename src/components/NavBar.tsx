12d9cb180d0324686bda216a0058f997cb05bc6b
Nguyen Hoang Thach <t3ngho00@students.oamk.fi>
2025-01-14 18:27:49 +0200

    Add color mode button

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,8 +4,8 @@ import { ColorModeButton } from './ui/color-mode'
 const NavBar = () => {
   return (
     <HStack justifyContent={"space-between"}>
-        <Image src={logo} boxSize='60px' />
-        <ColorModeButton></ColorModeButton>
+        <Image src={logo} boxSize='60px' bgColor={"white"}/>
+        <ColorModeButton marginRight={"10px"}></ColorModeButton>
     </HStack>
   )
 }
