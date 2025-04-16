92ccfb59df717364f89525bd9952338836e217df
Nguyen Hoang Thach <t3ngho00@students.oamk.fi>
2025-04-16 15:08:49 +0300

    Minor fixes

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
+import { SimpleGrid, Spinner, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
@@ -16,7 +16,6 @@ const GameGrid = ({ gameQuery }: Props) => {
     data,
     error,
     isLoading,
-    isFetchingNextPage,
     hasNextPage,
     fetchNextPage,
   } = useGames(gameQuery);
